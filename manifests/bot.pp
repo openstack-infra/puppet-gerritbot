97fbc089f0036ebf3267ef12a2f571ceaf790bf7
Ben Kero <ben.kero@gmail.com>
2016-08-15 11:06:11 -0700

    Refactor bot into defined types to allow multiple bots
    The Openstack-Infra project recently hit the limit of channels that the
    gerritbot can be joined on Freenode. As discussed, the desired action
    for here is to spin up a 'gerribot2'. However, due to how the
    puppet-gerritbot module was constructed, this is not quite simple.
    
    This commit refactors the 'bot' in this module to be a defined type,
    supporting multiple init scripts, config files, and logging files. This
    way, a second 'gerritbot2' can be spun up to join more channels.
    
    Change-Id: I5a8cd48c13a9a750d0b5a301a3c7202c97ca9f22

---
+++ manifests/bot.pp
@@ -0,0 +1,60 @@
+define gerritbot::bot ($channel_file,
+  $password,
+  $server,
+  $user,
+  $nick = $title,
+  $vhost_name = $::fqdn) {
+
+  include ::gerritbot
+
+  file { "/etc/init.d/gerritbot-${nick}":
+    ensure  => present,
+    group   => 'root',
+    mode    => '0555',
+    owner   => 'root',
+    content => template('gerritbot/gerritbot.init.erb'),
+    require => Class['gerritbot']
+  }
+
+  service { "gerritbot-${name}":
+    ensure     => running,
+    enable     => true,
+    hasrestart => true,
+    require    => File["/etc/init.d/gerritbot-${nick}"],
+    subscribe  => [
+      Package['gerritbot'],
+      File["/etc/gerritbot/gerritbot-${name}.config"],
+      File["/etc/gerritbot/channel_config-${name}.yaml"],
+    ],
+  }
+
+  file { "/etc/gerritbot/channel_config-${name}.yaml":
+    ensure  => present,
+    group   => 'gerrit2',
+    mode    => '0440',
+    owner   => 'root',
+    replace => true,
+    require => Class['gerritbot'],
+    content => $channel_file,
+  }
+
+  file { "/etc/gerritbot/logging-${name}.config":
+    ensure  => present,
+    group   => 'gerrit2',
+    mode    => '0440',
+    owner   => 'root',
+    replace => true,
+    require => Class['gerritbot'],
+    content => template('gerritbot/logging.config.erb'),
+  }
+
+  file { "/etc/gerritbot/gerritbot-${name}.config":
+    ensure  => present,
+    group   => 'gerrit2',
+    mode    => '0440',
+    owner   => 'root',
+    replace => true,
+    require => Class['gerritbot'],
+    content => template('gerritbot/gerritbot.config.erb')
+  }
+}
