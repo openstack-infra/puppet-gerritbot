9a389d9e55d2f7605f453f8e62245a4e8e5f4f06
Ben Kero <ben.kero@gmail.com>
2016-08-15 11:06:11 -0700

    Refactor bot into defined types to allow multiple bots
    The Openstack-Infra project recently hit the limit of channels that the
    gerritbot can be joined on Freenode. As discussed, the desired action
    for here is to spin up a 'gerribot2'. However, due to how the
    puppet-gerritbot module was constructed, this is not quite simple.
    
    This commit refactors the 'bot' in this module to be a defined type,
    supporting multiple init scripts, config files, and logging files. This
    way, a second 'gerritbot2' can be spun up to join more channels.
    
    Change-Id: I5a8cd48c13a9a750d0b5a301a3c7202c97ca9f22

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,14 +1,8 @@
 # == Class: gerritbot
 #
 class gerritbot(
-  $channel_file,
-  $nick,
-  $password,
-  $server,
-  $user,
   $ssh_rsa_key_contents    = undef,
   $ssh_rsa_pubkey_contents = undef,
-  $vhost_name              = $::fqdn,
 ) {
   include ::pip
 
@@ -18,66 +12,16 @@ class gerritbot(
     require  => Class['pip'],
   }
 
-  file { '/etc/init.d/gerritbot':
-    ensure  => present,
-    group   => 'root',
-    mode    => '0555',
-    owner   => 'root',
-    require => Package['gerritbot'],
-    source  => 'puppet:///modules/gerritbot/gerritbot.init',
-  }
-
-  service { 'gerritbot':
-    ensure     => running,
-    enable     => true,
-    hasrestart => true,
-    require    => File['/etc/init.d/gerritbot'],
-    subscribe  => [
-      Package['gerritbot'],
-      File['/etc/gerritbot/gerritbot.config'],
-      File['/etc/gerritbot/channel_config.yaml'],
-    ],
-  }
-
   file { '/etc/gerritbot':
     ensure => directory,
   }
 
   file { '/var/log/gerritbot':
-    ensure => directory,
-    group  => 'gerrit2',
-    mode   => '0775',
-    owner  => 'root',
-  }
-
-  file { '/etc/gerritbot/channel_config.yaml':
-    ensure  => present,
+    ensure  => directory,
     group   => 'gerrit2',
-    mode    => '0440',
+    mode    => '0775',
     owner   => 'root',
-    replace => true,
-    require => User['gerrit2'],
-    source  => $channel_file,
-  }
-
-  file { '/etc/gerritbot/logging.config':
-    ensure  => present,
-    group   => 'gerrit2',
-    mode    => '0440',
-    owner   => 'root',
-    replace => true,
-    require => User['gerrit2'],
-    source  => 'puppet:///modules/gerritbot/logging.config',
-  }
-
-  file { '/etc/gerritbot/gerritbot.config':
-    ensure  => present,
-    content => template('gerritbot/gerritbot.config.erb'),
-    group   => 'gerrit2',
-    mode    => '0440',
-    owner   => 'root',
-    replace => true,
-    require => User['gerrit2'],
+    require => Package['gerritbot'],
   }
 
   if $ssh_rsa_key_contents != undef {
